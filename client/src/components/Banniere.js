63d3df17bc87e1b6d441b6db6503c66a720bcc9a
BenjCaro <carbenjamin@gmail.com>
2025-04-27 15:43:41 +0200

    installation react, bootstrap, sass et express + mise en place arborescene app (client et server)

---
+++ client/src/components/Banniere.js
@@ -0,0 +1,11 @@
+ import banniere from '../assets/images/img-banniere.png';
+ 
+ const Banniere = () => {
+    return (
+        <>
+        <img alt='banniere' src={banniere} className='img-fluid'/>
+        </>
+    );
+ };
+
+ export default Banniere
