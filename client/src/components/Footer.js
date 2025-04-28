eacb817b35cec8da0789bac692286b917ab24a9c
BenjCaro <carbenjamin@gmail.com>
2025-04-28 11:45:29 +0200

    creation footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,51 @@
+import './Footer.scss';
+import logo from '../assets/logo/Logo.png';
+
+
+const Footer = () => {
+    return (
+        <>
+
+          <footer className='container-fluid'>
+            <div className='d-flex justify-content-evenly pt-3'>
+                <div>
+                <img src={logo} className='footer-logo'/>
+                </div>
+
+                <div>
+                    <h2 className='footer-adress'>Lyon</h2>
+                    <p className='footer-adress'> 
+                        
+                        101 cours Charlemagne  <br/>
+                        CS 20033  <br/>
+                        69269 LYON CEDEX 02  <br/>
+                        France  <br/>
+                        +33 (0)4 26 73 40 00 </p>
+                </div>
+            </div>
+            <hr/>
+            <div className='d-flex justify-content-center mt-3'>
+                <ul className='footer-menu d-flex'>
+                    <li className='ms-3'>
+                        <a href='#' className='footer-link'>Mentions Légales</a>
+                    </li>
+                    <li className='ms-3'>
+                        <a href='#' className='footer-link'>Données Personnelles</a>
+                    </li>
+                    <li className='ms-3'>
+                        <a href='#' className='footer-link'>Accessibilités</a>
+                    </li>
+                    <li className='ms-3'>
+                        <a href='#' className='footer-link'>Politiques des Cookies</a>
+                    </li>
+                </ul>
+
+            </div>
+          </footer>
+            
+
+        </>
+    );
+};
+
+export default Footer
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <>       
     <nav class="navbar navbar-expand-lg menu">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"> <img src={logo} className='logo'/></a>
+            <a class="navbar-brand" href="#"> <img src={logo} className='menu-logo'/></a>
              <button class="navbar-toggler mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
