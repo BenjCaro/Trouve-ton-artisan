8eb4670014e6adadbb0b759183edda2286098e64
BenjCaro <carbenjamin@gmail.com>
2025-04-30 17:50:50 +0200

    mise en place formulaire de contact , et integration dans la page Artisan

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,29 +4,33 @@ import '../assets/css/form.scss';
 const Form = () => {
     return (
         <>
-            <form>
-                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nom</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Votre Nom"/>
-                </div>
-                <div class="mb-3">
-                    <label for="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" placeholder="Votre adresse mail" name="email"/>
-                </div>
-                <div class="mb-3">
-                    <label for="object" class="form-label">Objet</label>
-                    <select className="form-select" id='object' name='object' aria-label="Default select example">
-                        <option selected>Choissisez l'objet du message</option>
-                        <option value="1">Renseignements</option>
-                        <option value="2">Devis</option>
-                        <option value="3">Rendez-vous</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="message" className="form-label">Message</label>
-                    <textarea className="form-control" id="message" name='message' rows="3"></textarea>
-                </div>
-            </form>
+                <form className='form-contact row mt-3 p-3' method='post'>
+                    <fieldset className="mb-3 mt-3">
+                        <label for="name" className="form-label fw-semibold">Nom</label>
+                        <input type="email" className="form-items form-control" id="name" placeholder="Votre Nom" required/>
+                    </fieldset>
+                    <fieldset className="mb-3 ">
+                        <label for="email" className="form-label fw-semibold">Email</label>
+                        <input type="email" className="form-items form-control" id="email" placeholder="Votre adresse mail" name="email" required/>
+                    </fieldset>
+                    <fieldset className="mb-3">
+                        <label for="object" className="form-label fw-semibold">Objet</label>
+                        <select className="form-items form-select " id='object' name='object' aria-label="Default select example" required>
+                            <option selected>Choissisez l'objet du message</option>
+                            <option value="1">Renseignements</option>
+                            <option value="2">Devis</option>
+                            <option value="3">Rendez-vous</option>
+                        </select>
+                    </fieldset>
+                    <fieldset className="mb-3">
+                        <label for="message" className="form-label fw-semibold">Message</label>
+                        <textarea className="form-items form-control" id="message" name='message' rows="3" required> </textarea>
+                    </fieldset>
+                    <div className='d-flex gap-3 justify-content-center mb-3'>
+                        <button className='btn form-btn' type='submit'>Envoyer</button>
+                        <button className='btn form-btn' type='reset'>Effacer</button>
+                    </div>
+                </form>
         </>
     );
 
--- client/src/pages/Artisan.js
+++ client/src/pages/Artisan.js
@@ -17,7 +17,7 @@ const Artisan = () => {
                 <h1 className='main-title text-center'>Nom Artisan</h1>
                 <hr className='main-hr'/>
                 <section className='container mb-5'>
-                    <div className='d-flex .flex-row justify-content-center align-items-center mb-3 gap-3'>
+                    <div className='bloc-infos-artisan d-flex .flex-row justify-content-center align-items-center mb-3 gap-3'>
                         <div className='top-card d-flex flex-column justify-content-center'>
                             <h2 className='secondary-title ms-2'>Nom</h2>
                             <ul className='card-infos mt-3 ms-2'>
@@ -27,8 +27,8 @@ const Artisan = () => {
                                 <li>Localisation</li>
                             </ul>
                         </div>
-                        <div>
-                            <figure className='home-pictures'>
+                        <div className=''>
+                            <figure className='artisan-pictures'>
                                 <img src={work} className='img-fluid'/>
                             </figure>
                         </div> 
@@ -41,7 +41,7 @@ const Artisan = () => {
                     </p>
                     <hr className='main-hr'/>
                 </section>
-                <section className='container mb-5'>
+                <section className='container mb-5 d-flex flex-column align-items-center'>
                 <h2 className='section-title text-center ms-2'>Contact Nom Artisan</h2>
                    {/* composant Form */}
                    <Form/>
