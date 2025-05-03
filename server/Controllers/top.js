8a16451b0610de84eefe5a06da5cf7db45e0bd97
BenjCaro <carbenjamin@gmail.com>
2025-05-03 20:33:22 +0200

    mise en place route top Artisan => donne le top 3 des artisans les mieux notés

---
+++ server/Controllers/top.js
@@ -0,0 +1,23 @@
+const {Artisan, Specialite} = require('../Models');
+
+
+exports.topArtisan = async (req, res) => {
+    try {
+       
+        const artisan = await Artisan.findAll({
+            attributes: ['nom_artisan', 'note', 'ville'],
+            where: {
+               top_artisan: true
+            },
+            include: {
+            model: Specialite,
+            attributes: ['nom_specialite']
+             }
+            });
+            
+            res.json(artisan);
+         
+    } catch (error) {
+        res.status(500).json({message : 'Erreur'});
+    }
+ };
--- server/server.js
+++ server/server.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const path = require('path');
-
 const { connectDB, sequelize } = require('./db/database');
-const {Artisan, Specialite, Categorie} = require('./Models');
+const topRouter = require('./Routes/topRoute');
 
 
 
 connectDB();
 const app = express();
-
 app.use(express.static(path.join(__dirname, '../client/build')));
-
-
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-// Affiche le top 3 des Artisans
-app.get('/api/artisan', async (req, res) => {
-   const artisan = await Artisan.findAll({
-    attributes: ['nom_artisan', 'note', 'ville'],
-    where: {
-      top_artisan: true
-    },
-    include: {
-      model: Specialite,
-      attributes: ['nom_specialite']
-    }
-   });
-   res.json(artisan);
-})
+app.use('/api/top-artisan', topRouter);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
