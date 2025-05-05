e72215573fc9aa4b7e0bfd17a7aff82eb78302c7
BenjCaro <carbenjamin@gmail.com>
2025-05-05 12:37:59 +0200

    modif fct getAllArtisanByCategorie

--- server/Controllers/categorie.js
+++ server/Controllers/categorie.js
@@ -3,23 +3,21 @@ const {Artisan, Specialite, Categorie} = require('../Models');
 exports.getAllArtisanByCategorie = async (req, res) => {
     try {
          
-        const artisan = await Artisan.findAll({
-            attributes: ['nom_artisan', 'note', 'ville'],
+
+        const categorie = await Categorie.findAll({
+            where: { nom_categorie: 'alimentation' },
+            attributes: ['nom_categorie'],
             include: [{
                 model: Specialite,
                 attributes: ['nom_specialite'],
                 include: [{
-                    model: Categorie,
-                    attributes: ['nom_categorie'],
-                    where: 
-                       {nom_categorie: 'alimentation'} // lier req.params ici 
+                    model: Artisan,
+                    attributes: ['nom_artisan']
                 }]
-
-            }],
-                 where: {'$Specialite.Categorie.nom_categorie$' : 'alimentation'} // lier req.params ici 
+            }]
         });
 
-        res.json(artisan);
+         res.json(categorie);
 
     } catch {
         res.status(500).json({message : 'Erreur'});
