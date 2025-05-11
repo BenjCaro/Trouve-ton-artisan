4356ab333f853fe23024f405cae01b1a814f9179
BenjCaro <carbenjamin@gmail.com>
2025-05-11 15:05:50 +0200

    mise en place page notfound

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -3,9 +3,10 @@ import Breadcrumb from '../components/Breadcrumb';
 import Footer from '../components/Footer';
 import error from '../assets/images/erreur 404.jpg';
 import { Link } from "react-router-dom";
-// import '../assets/css/notfound.scss';
+
 
 const NotFound = () => {
+
     return (
         <>
         <header>
@@ -13,15 +14,12 @@ const NotFound = () => {
         </header>
         <main>
             <Breadcrumb/>
-            <div>
-                <h1 className='main-title text-center'>Erreur 404</h1>
-                <p className='ms-5 mb-4'>Cette page n'existe pas.</p>
-            </div>
+            <h1 className='main-title text-center'>Erreur 404</h1>
             <figure className='container mt-4 mb-4'>
                 <img className='img-fluid' src={error}/>
             </figure>
-            <div className='d-flex justify-content-end'>
-                <button className='mt-4 mb-4 me-4'><Link to='/'>Retour Accueil</Link></button>
+            <div className='mt-4 mb-4 d-flex justify-content-end'>
+                <Link className='me-4' to='/'>Retour Accueil</Link>
             </div>
         </main>
         <Footer/>
